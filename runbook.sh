2232b59d05b1204b51632063fb4709ef6d39e073
Yongjian(Ken) Ouyang <yongjian.ouyang@outlook.com>
2019-04-16 16:51:03 -0500

    Updated runbook.sh

--- runbook.sh
+++ runbook.sh
@@ -10,24 +10,38 @@ for file in *; do
     mv "$file.temp" "$file"
 done
 
-sudo docker build -t bigdataplot/job-monitor:2.01 .
+sudo docker build -t bigdataplot/job-monitor:2.02 .
 
 sudo docker login --username bigdataplot
-sudo docker push bigdataplot/job-monitor:2.01
+sudo docker push bigdataplot/job-monitor:2.02
 
 
 ## ======================================== ##
 ##                  Run
 ## ======================================== ##
 
-sudo docker run --name job-monitor \
-    --detach \
+
+sudo docker run --detach \
+    --name job-monitor \
+    --hostname job-monitor \
     --restart always\
-    --publish 9011:80 \
+    --network host \
+    --env TERM=xterm \
+    --volume /etc/localtime:/etc/localtime:ro \
+    bigdataplot/job-monitor:2.02
+
+
+sudo docker run --detach \
+    --name job-monitor \
+    --hostname job-monitor \
+    --restart always\
+    --publish 9011:9011 \
     --publish 9001:9001 \
     --env TERM=xterm \
     --volume /etc/localtime:/etc/localtime:ro \
-    bigdataplot/job-monitor:2.01
+    bigdataplot/job-monitor:2.02
+
+
 
 
 sudo docker exec -it job-monitor bash
@@ -37,6 +51,12 @@ sudo docker exec -it job-monitor bash
 sed -i "s|__support_email__|your@email.com|g" ./application/views/welcome.php
 sed -i "s|__support_name__|Your Job Center|g" ./application/views/welcome.php
 
+sed -i "s|__support_email__|ken.oyang@gmail.com|g" ./application/views/welcome.php
+sed -i "s|__support_name__|BigDataPlot Job Center|g" ./application/views/welcome.php
+
+## ======================================== ##
+#  Web Front Page:
+nano /local/apps/job-monitor/application/config/supervisor.php
 
 ## ======================================== ##
 #  Configure URL :
@@ -59,3 +79,38 @@ $config['proxy_ips'] = '127.0.0.1';
         proxy_connect_timeout 86400;
     }
 
+
+=======================
+# On ks-db
+# Auto run autossh
+
+• Copy id_rsa to /root/.ssh/ folder and make sure it has the proper permission set
+• Create a service bash script on: cd /etc/systemd/system:
+sudo su
+
+Test:
+/usr/bin/autossh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -v -NR 19011:localhost:9011 youyang@104.42.171.155 -p 22 -i /root/.ssh/RSH-id_rsa
+
+
+Add file:
+nano /etc/systemd/system/bdpjob.service
+
+------------------------------------
+[Unit]
+Description=BigDataPlot Boot-up Service
+After=network.target
+
+[Service]
+Environment="AUTOSSH_GATETIME=0"
+ExecStart=/usr/bin/autossh -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -v -NR 19011:localhost:9011 youyang@104.42.171.155 -p 22 -i /root/.ssh/RSH-id_rsa
+
+[Install]
+WantedBy=multi-user.target
+------------------------------------
+
+• Apply the new service using:
+systemctl start bdpjob
+systemctl enable bdpjob
+systemctl stop bdpjob
+systemctl disable bdpjob
+systemctl status bdpjob
